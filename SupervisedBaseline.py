f447ff66f55357c553be55eae31e5daea78a953b
Xuhan Zhao <sherryzhao00@gmail.com>
2023-11-26 18:26:22 -0500

    Update SupervisedBaseline.py

--- SupervisedBaseline.py
+++ SupervisedBaseline.py
@@ -3,6 +3,7 @@ import torchvision
 import torchvision.transforms as transforms
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model_name = "ResNet18"
 num_epochs = 10
 batch_size = 64
 learning_rate = 0.001
@@ -15,22 +16,26 @@ def load_model(model_name):
     
 def load_ds(path_to_train, path_to_valid):
     transform = transforms.Compose([
-        transforms.Resize(256),
-        transforms.RandomHorizontalFlip(),
-        transforms.RandomVerticalFlip(),
-        transforms.RandomRotation(degrees=45),
-        transforms.ColorJitter(brightness=0.5, contrast=0.5, saturation=0.5, hue=0.5),
-        transforms.CenterCrop(224),
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        # transforms.Resize(256),
+        # transforms.RandomHorizontalFlip(),
+        # transforms.RandomVerticalFlip(),
+        # transforms.RandomRotation(degrees=45),
+        # transforms.ColorJitter(brightness=0.5, contrast=0.5, saturation=0.5, hue=0.5),
+        # transforms.CenterCrop(224),
+        transforms.ToTensor()
+        # transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ])
-    train_dataset = torchvision.datasets.ImageFolder(
+    train_dataset = torchvision.datasets.CIFAR10(
         root=path_to_train, 
-        transform=transform
+        transform=transform,
+        download=True
     )
-    valid_dataset = torchvision.datasets.ImageFolder(
-        root=path_to_valid
+    valid_dataset = torchvision.datasets.CIFAR10(
+        root=path_to_valid,
+        transform=transform,
+        download=True
     )
+    print("Datasets successfully loaded")
     return train_dataset, valid_dataset
 
 def train_model(model, train, valid):
@@ -41,6 +46,7 @@ def train_model(model, train, valid):
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
     for epoch in range(num_epochs):
+        print(f'Begin training Epoch {epoch+1}/{num_epochs}')
         acc_loss = 0
         for inputs, labels in train:
             inputs = inputs.to(device)
@@ -58,6 +64,7 @@ def train_model(model, train, valid):
         valid_loss = valid_model(model, valid)
         print(f'Epoch {epoch+1}/{num_epochs}, Validation Loss: {valid_loss:.4f}')
 
+    # save the model parameters
     torch.save(model.state_dict(), str(num_epochs) +  '_' + str(batch_size) + '_' + str(learning_rate) + '.pth')
 
 def valid_model(model, valid):
@@ -74,8 +81,8 @@ def valid_model(model, valid):
     
     return acc_loss
 
-model = load_model("ResNet18")
-train_ds, valid_ds = load_ds()
-train_loader = torch.utils.data.DataLoader(train_ds, batch_size=batch_size, shuffle=True, num_workers=2)
-valid_loader = torch.utils.data.DataLoader(valid_ds, batch_size=batch_size, shuffle=True, num_workers=2)
+model = load_model(model_name)
+train_ds, valid_ds = load_ds("./data", "./data")
+train_loader = torch.utils.data.DataLoader(train_ds, batch_size=batch_size, shuffle=True, num_workers=0)
+valid_loader = torch.utils.data.DataLoader(valid_ds, batch_size=batch_size, shuffle=True, num_workers=0)
 train_model(model, train_loader, valid_loader)
